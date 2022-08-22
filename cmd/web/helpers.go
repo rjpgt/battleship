fae41aa3ff837e18a70c446af3740f00d3f38c95
rjpgt <rjpgt@amrtam.in>
2022-08-22 21:11:10 +0530

    Minor changes to templates and template rendering

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -22,7 +22,7 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
-//  A convenience wrapper around clientError which sends a 404 Not Found response to the user.
+// A convenience wrapper around clientError which sends a 404 Not Found response to the user.
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
@@ -39,6 +39,7 @@ func (app *application) addDefaultData(td *templateData, r *http.Request, w http
 	}
 	return td
 }
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	ts, ok := app.templateCache[name]
 	if !ok {
@@ -48,7 +49,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	buf := new(bytes.Buffer)
 
-	err := ts.Execute(buf, app.addDefaultData(td, r, w))
+	err := ts.ExecuteTemplate(buf, "base", app.addDefaultData(td, r, w))
 	if err != nil {
 		app.serverError(w, err)
 		return
